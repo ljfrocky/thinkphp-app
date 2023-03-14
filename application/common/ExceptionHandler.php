c359ddec774c3078a2ccebe400842844062f10fd
ljfrocky <ljfrocky@gmail.com>
2023-03-14 17:27:41 +0800

    ajax请求发生错误时，返回json格式的数据，而不是html错误页面

--- application/common/ExceptionHandler.php
+++ application/common/ExceptionHandler.php
@@ -5,9 +5,38 @@
 
 namespace app\common;
 
+use Exception;
 use think\exception\Handle;
+use think\facade\App;
 
 class ExceptionHandler extends Handle
 {
     protected $ignoreReport = [];
+
+    public function render(Exception $e)
+    {
+        if (request()->isAjax()) {
+            $debugData = [];
+            if (App::isDebug()) {
+                $debugData = [
+                    'debug_file'  => $e->getFile(),
+                    'debug_line'  => $e->getLine(),
+                    'debug_trace' => $e->getTrace(),
+                ];
+            }
+            $errCode = $e->getCode();
+            if ($errCode == 0) {
+                $errCode = 500;
+            }
+            $data = [
+                'status'  => 0,
+                'code'    => $errCode,
+                'message' => $e->getMessage(),
+                'data'    => [],
+            ];
+            $data = array_merge($data, $debugData);
+            return json($data, 500);
+        }
+        return parent::render($e);
+    }
 }
